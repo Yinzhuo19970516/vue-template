6db4724ee21a20d094c1ef8b2139b39b5742f424
尹卓 <yinzhuo220422@credithc.com>
2022-08-24 20:19:24 +0800

    修改配置

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -25,7 +25,8 @@ export default {
     siteTitle: 'vue-template',
     nav: [
       { text: '首页', link: '/' },
-      { text: '项目简介', link: '/page/index' },
+      { text: '每日一题', link: '/meeting/index' },
+      { text: '项目简介', link: '/page/index.html' },
       { text: '更新日志', link: 'https://github.com/Yinzhuo19970516/vue-template/blob/main/CHANGELOG.md' }
     ],
     docFooter: {
@@ -33,26 +34,28 @@ export default {
       next: '下一节'
     },
     // 侧边导航
-    sidebar: [
-      {
-        text: '项目简介',
-        items: [
-          { text: '为什么要写这个项目', link: '/page/why' },
-          { text: '使用pnpm管理包', link: '/page/pnpm' },
-          { text: '多入口打包', link: '/page/pages' },
-          { text: '自动化生成项目基本模版', link: '/page/init' },
-          { text: 'Pinia',link:'/page/pinia'},
-          { text: '数据持久化',link:'/page/piniaPlugin'},
-          { text: 'axios二次封装',link:'/page/axios'},
-          { text: '路由跳转动画',link:'/page/router'},
-          { text: 'less sass的优化处理',link:'/page/less'},
-          { text: 'viewport适配方案',link:'/page/viewport'},
-          { text: '环境变量',link:'/page/env'},
-          { text: '兼容性处理方案',link:'/page/babel'},
-          { text: '总结', link: '/page/all' },
-        ]
-      }
-    ],
+    sidebar:{
+      "/page/": [
+        {
+          text: '项目简介',
+          items: [
+            { text: '为什么要写这个项目', link: '/page/why' },
+            { text: '使用pnpm管理包', link: '/page/pnpm' },
+            { text: '多入口打包', link: '/page/pages' },
+            { text: '自动化生成项目基本模版', link: '/page/init' },
+            { text: 'Pinia',link:'/page/pinia'},
+            { text: '数据持久化',link:'/page/piniaPlugin'},
+            { text: 'axios二次封装',link:'/page/axios'},
+            { text: '路由跳转动画',link:'/page/router'},
+            { text: 'less sass的优化处理',link:'/page/less'},
+            { text: 'viewport适配方案',link:'/page/viewport'},
+            { text: '环境变量',link:'/page/env'},
+            { text: '兼容性处理方案',link:'/page/babel'},
+            { text: '总结', link: '/page/all' },
+          ]
+        }
+      ],
+    },
     outlineTitle: '目录'
   }
 }
