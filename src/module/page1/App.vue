7840b0478e1cf0ca27131fd520b5785a86dde748
尹卓 <yinzhuo220422@credithc.com>
2022-09-15 20:45:55 +0800

    引入nprogress

--- src/module/page1/App.vue
+++ src/module/page1/App.vue
@@ -11,23 +11,32 @@
 <script setup>
 import { reactive } from 'vue'
 import { useRouter } from 'vue-router'
+import { start, close } from '@/common/utils/nprogress'
 
 const router = useRouter()
 const state = reactive({
   transitionName: 'slide-left'
 })
+
 router.beforeEach((to, from) => {
-  if (to.meta.level > from.meta.level) {
-    state.transitionName = 'slide-left'// 向左滑动
-  } else if (to.meta.level < from.meta.level) {
-    // 由次级到主级
-    state.transitionName = 'slide-right'// 向右滑动
-  } else {
-    state.transitionName = ''// 同级无过渡效果
-  }
+  start()
 })
+
+router.afterEach(to => {
+  close()
+})
+// router.beforeEach((to, from) => {
+//   if (to.meta.level > from.meta.level) {
+//     state.transitionName = 'slide-left'// 向左滑动
+//   } else if (to.meta.level < from.meta.level) {
+//     // 由次级到主级
+//     state.transitionName = 'slide-right'// 向右滑动
+//   } else {
+//     state.transitionName = ''// 同级无过渡效果
+//   }
+// })
 
 </script>
 <style>
-@import '@/common/style/pageTransAnimate.css';
+/*@import '@/common/style/pageTransAnimate.css';*/
 </style>
