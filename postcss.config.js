b54f7f344abb9d5da34c3d196579e5803938e83f
wswmsword <cjzjzzz@gmail.com>
2023-02-24 11:19:05 +0800

    chore: 替换适配方案为 postcss-mobile-to-multi-displays，提高全端可访问性

--- postcss.config.js
+++ postcss.config.js
@@ -6,33 +6,24 @@ module.exports = () => {
   return {
     plugins: {
       autoprefixer: {},
-      'postcss-px-to-viewport': {
-        // 要转化的单位
-        unitToConvert: 'px',
+      'postcss-mobile-to-multi-displays': {
         // 视口的宽度
         viewportWidth: 750,
         // 保留几位小数
         unitPrecision: 3,
         // 哪些属性需要修改
         propList: ['*'],
-        // 预期单位
-        viewportUnit: 'vw',
-        // 字体单位
-        fontViewportUnit: 'vw',
-        // 最小转化单位
-        minPixelValue: 1,
-        // 媒体查询里面的单位要不要转化
-        mediaQuery: true,
-        // 替换包含vw单位的
-        replace: true,
         // 排除转化文件
         exclude: [/node_modules/],
-        // 添加横向转化值
-        landscape: false,
-        // 横向采用的单位
-        landscapeUnit: 'vw',
-        // 横屏的视口宽度
-        landscapeWidth: false
+        // 移动端视图配置
+        mobileConfig: {
+          // 预期单位
+          viewportUnit: 'vw',
+          // 字体单位
+          fontViewportUnit: 'vw',
+          // 替换包含vw单位的
+          replace: true
+        }
       }
     }
   }
